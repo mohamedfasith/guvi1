2dc2063e10c8195d91b527192f01ad61e714e4d7
mohamedfasith <47409561+mohamedfasith@users.noreply.github.com>
2019-02-07 15:09:42 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,13 @@
+b1,b2,b3=input().split() 
+b1=int(b1)
+b2=int(b2)
+b3=int(b3)
+
+if (b1 > b2) and (b1 > b3):
+   largest =b1
+elif (b2 > b1) and (b2 > b3):
+   largest = b2
+else:
+   largest = b3
+ 
+print(largest)
